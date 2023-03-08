69026038058a6e2c4b0eb31a334f59c3e9e6c3c4
kimia <kajalafshari@yahoo.com>
2023-03-07 23:04:04 -0800

    removed utils

--- utils/coco.py
+++ utils/coco.py
@@ -19,7 +19,7 @@ import torch.utils.data
 from pycocotools import mask as coco_mask
 
 from .torchvision_datasets import CocoDetection as TvCocoDetection
-from util.misc import get_local_rank, get_local_size
+# from util.misc import get_local_rank, get_local_size
 import datasets.transforms as T
 import copy
 
